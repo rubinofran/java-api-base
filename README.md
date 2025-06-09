00f7a6e036254444d44381303c35a54fae06b6f4
ruinofran <rubinofranciscodev@gmail.com>
2025-06-09 11:00:44 -0300

    empty project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # java-api-base
+
+# Created with
+https://start.spring.io/
+
+# Configs 
+Some configurations in config.png
+
+# VSC configs
---
+++ src/main/java/com/example/api_base/ApiBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBaseApplication.class, args);
+	}
+
+}
